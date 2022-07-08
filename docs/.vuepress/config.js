30276a997d362dc884ff8abbf9909a08a713d740
Hiyiin <1963848756@qq.com>
2022-07-08 14:37:04 +0800

    2.1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,7 @@ module.exports = {
       { text: '首页', link: '/' },
       {
         text: '前端',
-        link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        link: '/pages/8309a5b876fc95e3/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
           // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
           {
@@ -39,7 +39,7 @@ module.exports = {
       },
       {
         text: '后端',
-        link: '/technology/',
+        link: '/note/Spring/',
         items: [
           {
             text: '基础',
@@ -60,7 +60,6 @@ module.exports = {
       },
       {
         text: '更多',
-        link: '/more/',
         items: [
           { text: '面试', link: '/pages/aea6571b7a8bae86/' },
           { text: '心情杂货', link: '/pages/d6d331/' },
---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,8 @@
+module.exports = {
+	'/guide/': require('../guide/sidebar'),
+
+	'/baodian/zero': require('../baodian/zero/sidebar'),
+	'/baodian/high': require('../baodian/high/sidebar'),
+
+	'/Spring': require('../baodian/high/sidebar'),
+}
