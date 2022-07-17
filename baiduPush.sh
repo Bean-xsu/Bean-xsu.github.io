f99738cf18f2ad22ec7d297ab490193c1b823916
Hiyiin <1963848756@qq.com>
2022-07-17 14:24:26 +0800

    更新部分笔记和markdown文档

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://hiyiin.github.io&token=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"
 
 rm -rf urls.txt # 删除文件
